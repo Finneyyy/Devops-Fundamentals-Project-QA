1ab92035331196dfbee0835393e8e7870760f488
finneyyy <eoin.finney@gmail.com>
2022-11-09 17:45:15 +0000

    added  pages, update routes

--- application/routes.py
+++ application/routes.py
@@ -6,3 +6,15 @@ from flask import render_template, request, redirect, url_for
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+@app.route('/add_book')
+def add_book():
+    return render_template('add_book.html')
+
+@app.route('/view_books')
+def view_books():
+    return render_template('/view_book.html')
+
+@app.route('/delete_books')
+def delete_books():
+    return render_template('/delete_book.html')

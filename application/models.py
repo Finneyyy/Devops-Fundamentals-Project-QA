f5a60f292960eb4dfd3261a79301470298af6f93
finneyyy <eoin.finney@gmail.com>
2022-11-09 15:54:15 +0000

    added two tables to models

--- application/models.py
+++ application/models.py
@@ -6,5 +6,8 @@ class Books(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     title=db.Column(db.String(50), nullable=False)
     
-#class Authors(db.Model):
-    
+class Authors(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    first_name=db.Column(db.String(30), nullable=False)
+    last_name=db.Column(db.String(30), nullable=False)
+    book_id=db.Column(db.Integer, db.ForeignKey('books.id'))

529330a917a79a208f8c00ebd9fb5e07849d3838
finneyyy <eoin.finney@gmail.com>
2022-11-09 16:59:25 +0000

    init routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,2 +1,8 @@
 from application import app, db
 from application.models import Books, Authors
+from flask import render_template, request, redirect, url_for
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')

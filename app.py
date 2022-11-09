899a79443cf9a79668b81b8d6fc3f598707cd6e3
finneyyy <eoin.finney@gmail.com>
2022-11-09 13:58:49 +0000

    basic app and outline created

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask
-
-app=Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return "There's going to be a library here soon"
+from application import app
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
